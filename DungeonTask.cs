fd0df919fc4a0ce0fdca6fb6c0c09bdffda65f85
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-06-10 00:49:21 +0300

    Implement FindShortestPath(); Upd README

--- DungeonTask.cs
+++ DungeonTask.cs
@@ -1,9 +1,75 @@
-﻿namespace Dungeon;
+﻿using Avalonia.Interactivity;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dungeon;
 
 public class DungeonTask
 {
 	public static MoveDirection[] FindShortestPath(Map map)
+	{        
+        Chest exit = new (map.Exit, 0);
+        var fromStartToExit = BfsTask.FindPaths(map, map.InitialPosition, new[] { exit }).FirstOrDefault();
+        if(fromStartToExit == null)
+        {
+            return new MoveDirection[0];
+        }
+
+        if (map.Chests.Length == 0)
+        {
+            return ConvertPointsToSteps(fromStartToExit);
+        }
+
+        Dictionary<Point, Chest> chests = map.Chests.Select(chest => chest).ToDictionary(k => k.Location);
+
+        var fromStartToChest = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
+        var fromChestToExit = BfsTask.FindPaths(map, map.Exit, map.Chests);
+
+        var fromStartToChestToExit = fromStartToChest
+            .Join(fromChestToExit, first => first.Value, second => second.Value, (first, second) => new
+            {
+                StartChest = first, ChestExit = second, Length = first.Length + second.Length -1
+            }).ToList();
+
+        if(fromStartToChestToExit.Count == 0)
+        {
+            return ConvertPointsToSteps(fromStartToExit);
+        }
+        var bestRoute = fromStartToChestToExit
+            .OrderBy(item => item.Length)
+            .ThenByDescending(item => chests[item.ChestExit.Value].Value)
+            .First();
+
+        MoveDirection[] result = ConvertPointsToSteps(MakePath(bestRoute.StartChest, bestRoute.ChestExit));
+        return result;
+    }
+
+    private static MoveDirection[] ConvertPointsToSteps(SinglyLinkedList<Point> route)
+    {
+        SinglyLinkedList<Point> currentPosition = route;
+        route = route.Previous;
+        List<MoveDirection> directions = new ();
+        while (route != null)
+        {
+            directions.Add(Walker.ConvertOffsetToDirection(currentPosition.Value - route.Value));
+            currentPosition = route;
+            route = route.Previous;
+        }
+
+        directions.Reverse();
+        return directions.ToArray();
+    }
+
+	private static SinglyLinkedList<Point> MakePath(SinglyLinkedList<Point> toChest, SinglyLinkedList<Point> toExit)
 	{
-		return new MoveDirection[0];
+		SinglyLinkedList<Point> result = toChest;
+		while(toExit.Previous != null)
+		{
+			toExit = toExit.Previous;
+			result = new(toExit.Value, result);
+		}
+		return result;
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Dungeon
+
+This project presents the workings of Bfs algorithm and
+uses it to find the best route through the dungeon.
+
+The best way means (1) the shortest way
+(2) with the most valuable chest lying on that way.
+
+If it is impossible to reach any chest and exit the dungeon with it,
+the best way equals the route from start to exit.
